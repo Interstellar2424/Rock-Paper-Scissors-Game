71534964bd18fd54e0282730158bfc85966d9ab8
Interstellar2424 <lakshyaahirr@gmail.com>
2024-07-01 13:37:10 +0530

    Add files via upload
    Added my files for the game

---
+++ Rock, Paper, Scissor!.py
@@ -0,0 +1,32 @@
+import random
+
+
+print("Rock, paper, Scissors!")
+
+
+RPS = ("rock", "paper", "scissors")
+#RPS stands for rock, paper, scissors
+
+player = input("choice (rock, paper, scissors): ")
+AI = random.choice(RPS)
+
+
+while player not in RPS:
+    player = input("choice (rock, paper, scissors): ")
+
+
+print(f"player: {player}")
+print(f"AI: {AI}")
+
+
+if player == AI:
+    print("it's a tie")
+elif player == "rock" and AI == "paper":
+    print("You Lose!")
+elif player == "paper" and AI == "pcissors":
+    print("You Lose!")
+elif player == "scissors" and AI == "rock":
+    print("You lose!")
+
+else:
+    print("You Won!")
